5abbe48749ff00da5283bde1a0acb6f61ba882fe
Tim.Simms <tim-simms@hotmail.com>
2025-04-25 19:20:12 +0100

    Heroku

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-import connectDB from "./mongodb/connection";
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import resolvers from "./graphql/resolvers/resolvers";
-import typeDefs from "./graphql/schemas/typeDefs";
+import dotenv from 'dotenv';
+import connectDB from './api/mongodb/connection';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import resolvers from './api/graphql/resolvers/resolvers';
+import typeDefs from './api/graphql/schemas/typeDefs';
 dotenv.config();
 const startServer = async (): Promise<void> => {
   try {
     if (!process.env.PORT) {
-      throw new Error("PORT is not defined in environment variables");
+      throw new Error('PORT is not defined in environment variables');
     }
     const PORT = process.env.PORT || 4000;
 
@@ -26,7 +26,7 @@ const startServer = async (): Promise<void> => {
 
     // Start Server
   } catch (error) {
-    console.error("Failed to start the server:", error);
+    console.error('Failed to start the server:', error);
   }
 };
 
